f814ae6a059adc79549524bc7bf335a96d336192
ShurmiNaz <shurminaz21.com>
2022-11-09 01:04:26 +0600

    add spreed cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Product.css';
 
-const Product = (props) => {
-
-
-     const { name, img, price, ratings, seller } = props.product;
+const Product = ({ product, handleAddToCart }) => {
+     // const { product, handleAddToCart } = props;
+     const { name, img, price, ratings, seller } = product;
      return (
           <div className="product">
                <img src={img} alt="" />
@@ -12,13 +11,11 @@ const Product = (props) => {
                     <h5 className='product-name'>{name}</h5>
                     <p className='product-price'>Price : ${price}</p>
                     <div className='product-other-info' >
-                         <small> Manufacturer : {seller}</small>
+                         <small> Seller : {seller}</small>
                          <br />
                          <small> Ratings : {ratings} stars</small>
                     </div>
-
-                    <button onClick={() => props.handleAddToCart(props.product)} className='product-button'>Add To Cart
-
+                    <button onClick={() => handleAddToCart(product)} className='product-button'>Add To Cart
                     </button>
                </div>
           </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,13 +4,15 @@ import './Shop.css';
 
 const Shop = () => {
      const [products, setProducts] = useState([]);
+     const [cart, setCart] = useState([]);
      useEffect(() => {
           fetch('products.json')
                .then(res => res.json())
                .then(data => setProducts(data))
      }, []);
      const handleAddToCart = (product) => {
-          console.log(product);
+          const newCart = [...cart, product];
+          setCart(newCart);
      }
      return (
           <div className="shop-container">
@@ -25,7 +27,7 @@ const Shop = () => {
 
                </div>
                <div className="cart-container">
-                    <h5>Order Summary</h5>
+                    <h5>Order Summary : {cart.length} </h5>
                </div>
 
           </div>
